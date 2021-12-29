ebb9d622d6d28219566ce7db1e360982c85690f8
vtpasquale <vtpasquale@gmail.com>
2021-12-29 18:29:21 -0500

    adding to MMD chapter 7 exercise 3 - in work

--- ch7ex3_mmd/ch7ex3_mmd.C
+++ ch7ex3_mmd/ch7ex3_mmd.C
@@ -55,11 +55,16 @@ int main(int argc, char *argv[])
     #include "createTime.H"
     #include "createMesh.H"
     
-    int faceIndex = 0;
-
+    // Hints
     // https://jibranhaider.com/blog/mesh-information-in-openfoam/
 
+    /*---------------------------------------------------------------------------*\
+    (a) Write a program to read the mesh and loop over all boundary patches. Then
+    for each patch print the centroid and normal vector of its faces.
+    \*---------------------------------------------------------------------------*/
+
     // Loop over boundary patches
+    int faceIndex = 0;
     forAll(mesh.boundary(), patch)
     {
         Info << "Patch name " << mesh.boundary()[patch].name() << endl;            // Boundary patch name
@@ -74,9 +79,57 @@ int main(int argc, char *argv[])
             << endl;
         }
     }
-
     Info << endl;
-    Info<< "End\n" << endl;
+    Info << "End\n" << endl;
+
+    /*---------------------------------------------------------------------------*\
+    (b) Modify the program to define a volumeScalarField T. Set the values for T at
+    element centroids and at the boundaries to 10x2y2, where x and y are the
+    coordinates of the element centroids and for the case of the boundary the
+    centroids of the boundary faces.
+    \*---------------------------------------------------------------------------*/
+    Info<< "Reading field T\n" << endl;
+	volScalarField T
+	(
+		IOobject
+		(
+		    "T",
+		    runTime.timeName(),
+		    mesh,
+		    IOobject::MUST_READ,
+		    IOobject::AUTO_WRITE
+		),
+		mesh
+	);
+
+    // Print T
+    Info << "volScalarField T = " << endl;
+    Info << T << nl << endl;
+
+    Info << "T.internalField().size() = " << T.internalField().size() << endl;
+    // forAll(T.(),i)
+    //    Info << T.iinternalFieldnternalField()[i] << endl;
+
+    // Print T
+    Info << "T..size() = " << T.size() << endl;
+    forAll(T,i)
+        Info << T[i] << endl;
+    Info << T << nl << endl;
+
+    // Update runtime
+    Info << runTime.time().value() << endl;
+    runTime.loop();
+    Info << runTime.time().value() << endl;
+    Info << T << nl << endl;
+
+    // Update T
+    const volVectorField& C = mesh.C(); // Cell center coordinates
+    forAll(T,i)
+    {
+        T[i] = 10.0*C[i][0]*C[i][0]*C[i][1]*C[i][1];
+        Info << T[i] << endl;
+    }
+    runTime.writeNow();
 
     return 0;
 }
